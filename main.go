6199627650eddb594c80a3a1517c479ad6e7071a
ridhamu <ridhauwu@gmail.com>
2026-05-27 21:08:52 +0700

    done creating handler

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("this is home handler!"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("displaying snippet view page"))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("displaying form for creating snippet"))
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	// our server mux, which we can use to connect certain end point/url path to certain hendlers
+	// for example /home is for Home hanlders
+	mux := http.NewServeMux()
+	mux.HandleFunc("/{$}", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
+
+	log.Println("server is running on port :4000")
+	err := http.ListenAndServe(":4000", mux)
+	if err != nil {
+		log.Println(err)
+		return
+	}
 }
