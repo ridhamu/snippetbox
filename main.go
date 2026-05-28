c29eace2e1459cc6666fa6e795cb5cb63847bf5b
ridhamu <ridhauwu@gmail.com>
2026-05-28 22:32:46 +0700

    next: writing response body

--- main.go
+++ main.go
@@ -1,29 +1,44 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Server", "GO")
 	w.Write([]byte("this is home handler!"))
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("displaying snippet view page"))
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	msg := fmt.Sprintf("Displaying snippetView for id: %d\n", id)
+	w.Write([]byte(msg))
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("displaying form for creating snippet"))
 }
 
+func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("Saving a snippet..."))
+}
+
 func main() {
 	// our server mux, which we can use to connect certain end point/url path to certain hendlers
 	// for example /home is for Home hanlders
 	mux := http.NewServeMux()
-	mux.HandleFunc("/{$}", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
+	mux.HandleFunc("GET /{$}", home)
+	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
+	mux.HandleFunc("GET /snippet/create", snippetCreate)
+	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
 	log.Println("server is running on port :4000")
 	err := http.ListenAndServe(":4000", mux)
